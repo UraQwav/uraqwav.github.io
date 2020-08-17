2a41b635c021c4e5d102d593ed23542f52ed9cf5
uraqwav <ura.qwav@gmail.com>
2020-08-18 02:27:40 +0300

    add Black Theme:)

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, Directive, Input } from '@angular/core';
 import { trigger, transition, animate, style } from '@angular/animations';
 
 @Component({
@@ -13,15 +13,40 @@ import { trigger, transition, animate, style } from '@angular/animations';
      ])
   ]
 })
+
 export class HeaderComponent implements OnInit {
 
   headerHello:boolean;
-
-  constructor() {
+  changeThemeColor:boolean;
+  ategThemeCollor;
+  ThemeCollorBackground;
+  constructor(private elementRef: ElementRef) {
     this.headerHello = false;
-   }
-
+    this.changeThemeColor = false;
+  }
   ngOnInit(): void {
   }
-
+  changeTheme(){
+    if(this.changeThemeColor == false){
+      this.changeThemeColor = true;
+      setTimeout(()=>this.blackTheme(),712);
+      this.ThemeCollorBackground = '#36383b';
+    }
+    else{
+      this.changeThemeColor = false;
+      setTimeout(()=>this.whiteTheme(),712);
+      this.ThemeCollorBackground = '#fff';
+    }
+  }
+  whiteTheme():any{
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#fff';
+    this.elementRef.nativeElement.ownerDocument.body.style.color = '';
+    this.ategThemeCollor = '';
+  }
+  blackTheme():any{
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#36383b';
+    this.elementRef.nativeElement.ownerDocument.body.style.color = '#fff';
+    this.ategThemeCollor = '#fff';
+  }
+ 
 }
